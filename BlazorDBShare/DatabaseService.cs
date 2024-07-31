3d150195b8e725516ffa45550c051d6010ff4fbe
상욱 박 <sanguk1103@naver.com>
2024-07-31 16:18:45 +0900

    초기 커밋.

---
+++ BlazorDBShare/DatabaseService.cs
@@ -0,0 +1,67 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+public class DatabaseService
+{
+    private readonly string _connectionString;
+
+    public DatabaseService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public DataTable ExecuteQuery(string query)
+    {
+        using (var connection = new OleDbConnection(_connectionString))
+        {
+            connection.Open();
+            using (var command = new OleDbCommand(query, connection))
+            {
+                using (var adapter = new OleDbDataAdapter(command))
+                {
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+
+    public List<string> GetTableNames()
+    {
+        var tables = new List<string>();
+        using (var connection = new OleDbConnection(_connectionString))
+        {
+            connection.Open();
+            var schema = connection.GetSchema("Tables");
+            foreach (DataRow row in schema.Rows)
+            {
+                string tableName = row["TABLE_NAME"].ToString();
+                if (!tableName.StartsWith("MSys"))
+                {
+                    tables.Add(tableName);
+                }
+            }
+        }
+        return tables;
+    }
+
+    public int ExecuteNonQuery(string query, Dictionary<string, object> parameters = null)
+    {
+        using (var connection = new OleDbConnection(_connectionString))
+        {
+            connection.Open();
+            using (var command = new OleDbCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.AddWithValue(param.Key, param.Value);
+                    }
+                }
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ BlazorDBShare/Program.cs
@@ -0,0 +1,31 @@
+using BlazorDBShare;
+using BlazorDBShare.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<DatabaseService>(sp =>
+    new DatabaseService("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Autobase\\Database1.mdb;"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
